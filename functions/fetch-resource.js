e27c62022d10f0c320e8310cf097773bc93dc726
osaid <redfirerpk@gmail.com>
2024-01-04 06:37:55 +0300

    managed to get rid of all the errors and actually get netlify functions working

--- functions/fetch-resource.js
+++ functions/fetch-resource.js
@@ -3,23 +3,23 @@ const apiKey = process.env.HADITH_API_KEY;
 
 exports.handler = async (event, context) => {
   try {
-    // console.log(event, "here we go", process.env);
-    // const requestedResource = event.queryStringParameters.resource.replace(
-    //   "slash",
-    //   "/"
-    // );
+    const dividedBySlashes = event.path.split("/");
+    const index = event.path.indexOf(dividedBySlashes[4]);
+    const requestedResource = event.path.slice(index);
+    const query = event.queryStringParameters;
+    let fixedApiUrl;
 
-    const response = await fetch(
-      `https://hadithapi.com/api/sahih-bukhari/chapters?apiKey=${apiKey}`
-    );
-
-    console.log("past fetching");
-
-    if (!response.ok) {
-      throw new Error(`Network response was not ok, NETLIFY LO`);
+    if (requestedResource.includes("hadiths")) {
+      fixedApiUrl = `hadiths?apiKey=${apiKey}&book=${query.book}&chapter=${query.chapter}&paginate=${query.paginate}`;
+    } else {
+      fixedApiUrl = `${requestedResource}?apiKey=${apiKey}`;
     }
 
-    console.log("past checking if response is ok");
+    const response = await fetch(`https://hadithapi.com/api/${fixedApiUrl}`);
+
+    if (!response.ok) {
+      throw new Error(`Network response was not ok`);
+    }
 
     const data = await response.json();
 
@@ -31,9 +31,7 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 500,
       body: JSON.stringify({
-        error: `internal server error, netlify, ${JSON.stringify(
-          event
-        )}, "AND HERE WE GO", ${JSON.stringify(event).queryStringParameters}`,
+        error: `internal server error`,
       }),
     };
   }
